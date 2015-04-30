80bc2932c899267d0f8b407bc97fde928b76b398
emario <thetest3r@gmail.com>
2015-04-30 11:22:52 -0400

    Added EasterEgg from. It's all skeletal at this pt. Waiting for pictures

--- ListProcessingExcelPlugin/HelpDialog.Designer.cs
+++ ListProcessingExcelPlugin/HelpDialog.Designer.cs
@@ -38,7 +38,7 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.pictureBox4 = new System.Windows.Forms.PictureBox();
-            this.easterEggButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
@@ -144,22 +144,21 @@
             this.pictureBox4.TabIndex = 11;
             this.pictureBox4.TabStop = false;
             // 
-            // easterEggButton
+            // button1
             // 
-            this.easterEggButton.Location = new System.Drawing.Point(15, 476);
-            this.easterEggButton.Name = "easterEggButton";
-            this.easterEggButton.Size = new System.Drawing.Size(75, 23);
-            this.easterEggButton.TabIndex = 12;
-            this.easterEggButton.Text = "Easter Egg?";
-            this.easterEggButton.UseVisualStyleBackColor = true;
-            this.easterEggButton.Click += new System.EventHandler(this.easterEggButton_Click);
+            this.button1.Location = new System.Drawing.Point(41, 439);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // HelpDialog
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(477, 511);
-            this.Controls.Add(this.easterEggButton);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
@@ -196,7 +195,7 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.PictureBox pictureBox4;
-        private System.Windows.Forms.Button easterEggButton;
+        private System.Windows.Forms.Button button1;
 
     }
 }
